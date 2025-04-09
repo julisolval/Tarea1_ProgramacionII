608422df383d2edb07df1e0f33f92fe18bf4a8c0
Julissa Solano Valverde <julissa.solano.valverde@est.una.ac.cr>
2025-04-08 22:37:08 -0600

    Cambios en cpp

--- Tarea1_JulissaSolanoValverde/Anio.cpp
+++ Tarea1_JulissaSolanoValverde/Anio.cpp
@@ -1,5 +1,3 @@
-// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 #include "Anio.h"
 
 Anio::Anio() : anio{0} {}
--- Tarea1_JulissaSolanoValverde/Main.cpp
+++ Tarea1_JulissaSolanoValverde/Main.cpp
@@ -3,8 +3,6 @@ Nombre: Julissa Solano Valverde
 Cedula: 118630056
 Carne: 313821
 */
-// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 #include "InterfazPrincipal.h"
 
 int main() {
